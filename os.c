96e69abdaf5d00fd52055fec1e8c01ebc8d27626
ravi2248 <43957515+ravi2248@users.noreply.github.com>
2020-03-28 16:28:11 +0530

    Update os.c

--- os.c
+++ os.c
@@ -10,7 +10,7 @@ int main()
     	scanf("%d", &processes);
  
     	for (i = 0; i < processes; i++) 
-		  {
+	{
         	running[i] = 1;
         	counter++;
     	}
@@ -19,39 +19,39 @@ int main()
  
     	printf("\nEnter Maximum resources :\n");
     	for (i = 0; i < resources; i++) 
-		  { 
+	{ 
 	        scanf("%d", &maxres[i]);
     	}
  
    	printf("\nEnter Allocated Resource Table:\n");
     	for (i = 0; i < processes; i++) 
-	  	{
+	{
 	        for(j = 0; j < resources; j++) 
-			{
+		{
   			scanf("%d", &current[i][j]);
         	}
     	}
     	
    printf("\nEnter Maximum Reasources will be allocated :\n");
     	for (i = 0; i < processes; i++) 
-	  	{
+	{
         	for(j = 0; j < resources; j++) 
-			    {
+		{
             		scanf("%d", &max_claim[i][j]);
         	}
     	}
  
 	printf("\nTotal reasources should be : ");
     	for (i = 0; i < resources; i++) 
-	    {
+	{
 	        printf("\t%d", maxres[i]);
-	    }
+	}
  
     	printf("\nThe Allocated Resource Table:\n");
     	for (i = 0; i < processes; i++) 
 	    {
 	        for (j = 0; j < resources; j++) 
-		      {
+		{
             		printf("\t%d", current[i][j]);
         	}
 		      printf("\n");
@@ -59,25 +59,25 @@ int main()
  
     	printf("\nThe Maximum Reasources will be :\n");
     	for (i = 0; i < processes; i++) 
-	   	{
+	{
         	for (j = 0; j < resources; j++) 
-			    {
+		{
 		        printf("\t%d", max_claim[i][j]);
         	}
         	printf("\n");
     	}
  
     	for (i = 0; i < processes; i++) 
-		  {
+	{
         	for (j = 0; j < resources; j++) 
-			    {
+		{
             		allocation[j] += current[i][j];
         	}
     	}
  
     	printf("\nAllocated resources:");
     	for (i = 0; i < resources; i++) 
-	  	{
+	{
         	printf("\t%d", allocation[i]);
     	}
  
@@ -88,7 +88,7 @@ int main()
  
     	printf("\nAvailable resources:");
     	for (i = 0; i < resources; i++) 
-		  {
+	{
         	printf("\t%d", available[i]);
     	}
     	printf("\n");
@@ -99,73 +99,73 @@ int count = 1;
 while (counter != 0) 
 {
     for (i = 0; i < processes; i++) 
-	  {
-	    	safe[i] = 0;
-		    printf("Enter resources you want for Process(Request) :\n");
-		    for (k = 0; k < resources; k++) 
-	    	{
-		      	scanf("%d", &request[i][k]);
+    {
+	safe[i] = 0;
+	printf("Enter resources you want for Process(Request) :\n");
+	for (k = 0; k < resources; k++) 
+	{
+		scanf("%d", &request[i][k]);
         }
         if (running[i]) 
-		    {
+	{
             exec = 1;
             for (j = 0; j < resources; j++) 
-			      {
+	    {
                 if (request[i][j] != (max_claim[i][j] - current[i][j])) 
-				        {
+		{
                     exec = 0;
                     break;
                 }
-				        else if(request[i][j] > available[j])
-				        {
-					          exec = 0;
-                    break;
-				        }
+		else if(request[i][j] > available[j])
+		{
+			exec = 0;
+                    	break;
+		}
             }
 	
             if (exec) 
-			      {
+	    {
                 printf("\nProcess%d is executing\n", i + 1);
                 running[i] = 0;
                 counter--;
                 safe[i] = 1;
  				
                 for (j = 0; j < resources; j++) 
-				        {
+		{
                     available[j] += request[i][j];
                 }   
             }
             else
             {
             	printf("\nProcess%d is not executing\n",i+1);
-			      }
-			int z;
-			printf("\nAvailable vector:");
+	    }
+	  	int z;
+		printf("\nAvailable vector:");
        		for (z = 0; z < resources; z++) 
-			    {
-    	        printf("\t%d", available[z]);
-    	    }
-    	    printf("\n");
+		{
+    	        	printf("\t%d", available[z]);
+    	    	}
+    	    	printf("\n");
         }
     }
     for(i=0;i<processes;i++)
     {
     	if (!safe[i]) 
-		{
+	{
         	printf("\nThe processes are in unsafe state.\n");
         	count = 0;
         	break;
     	}
-	}
+    }
 	if(count==1)
 	{
         printf("\nThe process is in safe state");
         printf("\nAvailable vector:");
         for (i = 0; i < resources; i++) 
-		    {
+	{
             printf("\t%d", available[i]);
         }
-		    printf("\n");
+	printf("\n");
   }
     counter=0;
 }
